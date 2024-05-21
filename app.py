37872021368b19f7f0a4caa4ab052e10adfeaebc
Adida <adida.barack@student.moringaschool.com>
2024-05-21 10:43:44 +0300

    Added functions

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 def my_function (param):
     print("Running my_function")
     return param + 1
+
+first_name = "Barack"
+lat_name = "Adida"
+first_no = 35
+second_no = 1
+
+def say_hi(end="\n\n"):
+    print(f"Hi there, {first_name}. Is your name {lat_name}")
+
+say_hi()
+
+def stylish_painter(): 
+    best_hairstyle = "Bob Ross"
+    return "Barack-legend"
+    return best_hairstyle
+    print(best_hairstyle)
+
+stylish_painter()    
+
+def sum():
+    print(first_no + second_no)
+
+sum()
